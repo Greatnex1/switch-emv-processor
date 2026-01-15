bd1938dadd409d81a762f9f885931ed5c8d7851d
noah.akoni <noah.akoni@ubagroup.com>
2026-01-15 10:56:56 +0100

    add main

---
+++ src/main/java/com/ubagroup/switchemv/SwitchEmvApplication.java
@@ -0,0 +1,13 @@
+package com.ubagroup.switchemv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwitchEmvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwitchEmvApplication.class, args);
+    }
+
+}
